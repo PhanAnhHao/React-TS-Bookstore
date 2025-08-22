abd9abce77102024b25d2f9c7df5a26297813b28
Anh Hào Phan <pdkenny25@gmail.com>
2025-08-22 15:32:11 +0700

    setup react router

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom"
+import AppHeader from "./components/layout/app.header"
 
 function Layout() {
 
   return (
     <div>
-      Hello world with PhAHao
+      <AppHeader />
+      <Outlet />
     </div>
   )
 }
