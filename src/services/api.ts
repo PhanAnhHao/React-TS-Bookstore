b537ff7b300208f2cd7dfee4ca4d7c51787be08d
Anh Hào Phan <pdkenny25@gmail.com>
2025-08-26 22:18:27 +0700

    Display/Filter/Sort Books

--- src/services/api.ts
+++ src/services/api.ts
@@ -60,3 +60,14 @@ export const deleteUserAPI = (_id: string) => {
     const urlBackend = `/api/v1/user/${_id}`;
     return axios.delete<IBackendRes<IRegister>>(urlBackend)
 }
+
+export const getBooksAPI = (query: string) => {
+    const urlBackend = `/api/v1/book?${query}`;
+    return axios.get<IBackendRes<IModelPaginate<IBookTable>>>(urlBackend,
+        {
+            headers: {
+                delay: 100
+            }
+        }
+    )
+}
